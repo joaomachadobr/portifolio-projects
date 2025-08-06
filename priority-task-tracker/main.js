054b99ff9dd9127af49d97387a8c0442bbcebb2f
joaomachadobr <214250129+joaomachadobr@users.noreply.github.com>
2025-08-06 11:02:39 +0100

    feat: add first working task creation logic
    - Implemented 'add' function in JavaScript
    - Tasks are now added dynamically to the task list
    - Basic validation for empty input included

--- priority-task-tracker/main.js
+++ priority-task-tracker/main.js
@@ -1,1 +1,14 @@
-console.log('okay')
+let NewTask = document.querySelector('#new-task-input')
+let task = document.querySelector('.task-list')
+let list = []
+
+function add() {
+    if (NewTask.value == '') {
+        alert('campo vazio')
+    } else {
+        let item = document.createElement('li')
+        item.innerHTML = NewTask.value // adciona o conteudo dentro do no li
+        task.appendChild(item)
+        list.push(NewTask.value)
+    }
+}
